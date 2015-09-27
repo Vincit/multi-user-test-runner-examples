f1112354ddbfa38add0c4a5ef85560eb0995b0c8
Juha Siponen <juha.siponen@vincit.fi>
2015-09-27 19:10:51 +0300

    Add example test showing role aliasing

---
+++ src/test/java/fi/vincit/mutrproject/AbstractConfiguredRoleAliasIT.java
@@ -0,0 +1,65 @@
+package fi.vincit.mutrproject;
+
+
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+
+import fi.vincit.multiusertest.annotation.MultiUserTestConfig;
+import fi.vincit.multiusertest.runner.junit.MultiUserTestRunner;
+import fi.vincit.multiusertest.runner.junit.framework.SpringMultiUserTestClassRunner;
+import fi.vincit.multiusertest.test.AbstractUserRoleIT;
+import fi.vincit.multiusertest.util.LoginRole;
+import fi.vincit.mutrproject.config.SecurityConfig;
+import fi.vincit.mutrproject.domain.Role;
+import fi.vincit.mutrproject.domain.User;
+import fi.vincit.mutrproject.service.UserService;
+
+@TestExecutionListeners({DependencyInjectionTestExecutionListener.class,
+        DirtiesContextTestExecutionListener.class,
+        TransactionalTestExecutionListener.class})
+@MultiUserTestConfig(
+        runner = SpringMultiUserTestClassRunner.class,
+        defaultException = AccessDeniedException.class)
+@ContextConfiguration(classes = {Application.class, SecurityConfig.class})
+@RunWith(MultiUserTestRunner.class)
+public abstract class AbstractConfiguredRoleAliasIT extends AbstractUserRoleIT<User, Role> {
+
+    @After
+    public void clear() {
+        userService.clearUsers();
+    }
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void loginWithUser(User user) {
+        userService.loginUser(user);
+    }
+
+    @Override
+    protected User createUser(String username, String firstName, String lastName, Role userRole, LoginRole loginRole) {
+        return userService.createUser(username, userRole);
+    }
+
+    @Override
+    protected Role stringToRole(String role) {
+        if (role.equals("UNREGISTERED")) {
+            return Role.ROLE_ANONYMOUS;
+        } else {
+            return Role.valueOf("ROLE_" + role);
+        }
+    }
+
+    @Override
+    protected User getUserByUsername(String username) {
+        return userService.loadUserByUsername(username);
+    }
+}
---
+++ src/test/java/fi/vincit/mutrproject/service/TodoServiceRoleAliasIT.java
@@ -0,0 +1,50 @@
+package fi.vincit.mutrproject.service;
+
+import static fi.vincit.multiusertest.rule.Authentication.notToFail;
+import static fi.vincit.multiusertest.rule.Authentication.toFail;
+import static fi.vincit.multiusertest.util.UserIdentifiers.ifAnyOf;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fi.vincit.multiusertest.annotation.TestUsers;
+import fi.vincit.multiusertest.util.LoginRole;
+import fi.vincit.mutrproject.AbstractConfiguredRoleAliasIT;
+
+/**
+ * Example test using role aliasing. See {@link AbstractConfiguredRoleAliasIT} for an example
+ * how to implement role aliasing.
+ */
+@TestUsers(
+        creators = {"role:SUPER_ADMIN", "role:ADMIN", "role:USER", "role:USER"},
+        users = {"role:SUPER_ADMIN", "role:ADMIN", "role:USER", "role:UNREGISTERED", TestUsers.CREATOR}
+)
+public class TodoServiceRoleAliasIT extends AbstractConfiguredRoleAliasIT {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void getPrivateTodoList() throws Throwable {
+        long id = todoService.createTodoList("Test list", false);
+        logInAs(LoginRole.USER);
+        authorization().expect(toFail(ifAnyOf("role:USER", "role:UNREGISTERED")));
+        todoService.getTodoList(id);
+    }
+
+    @Test
+    public void getPublicTodoList() throws Throwable {
+        long id = todoService.createTodoList("Test list", true);
+        logInAs(LoginRole.USER);
+        todoService.getTodoList(id);
+    }
+
+    @Test
+    public void addTodoItem() throws Throwable {
+        long listId = todoService.createTodoList("Test list", false);
+        logInAs(LoginRole.USER);
+        authorization().expect(notToFail(ifAnyOf("role:ADMIN", "role:SUPER_ADMIN", TestUsers.CREATOR)));
+        todoService.addItemToList(listId, "Write tests");
+    }
+
+}
