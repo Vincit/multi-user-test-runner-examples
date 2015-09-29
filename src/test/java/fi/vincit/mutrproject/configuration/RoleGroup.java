5c1a28de55fe73a0f6ad82944d0352b6ef42572d
Juha Siponen <juha.siponen@vincit.fi>
2015-09-29 08:55:08 +0300

    Add Javadoc for RoleGroup

--- src/test/java/fi/vincit/mutrproject/configuration/RoleGroup.java
+++ src/test/java/fi/vincit/mutrproject/configuration/RoleGroup.java
@@ -1,5 +1,10 @@
 package fi.vincit.mutrproject.configuration;
 
+/**
+ * A custom RoleGroup which is used to implement
+ * multi role support. See {@link AbstractConfiguredMultiRoleIT}
+ * for how to configure multi role support.
+ */
 public enum RoleGroup {
     ADMINISTRATOR,
     REGULAR_USER
